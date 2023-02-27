22546a7e6cdd3b19e71d2f114d16ef585262fa1c
BRun0442 <brunoosmar442@gmail.com>
2023-02-27 15:10:35 -0300

    API running OK
    yarn start to start application

--- RH.sql
+++ RH.sql
@@ -1,26 +1,30 @@
 create database RH;
 use RH;
 
-create table usuario(
-	id_user int primary key auto_increment,
+#drop database RH;
+
+create table user(
+	id int primary key auto_increment,
+    password varchar(25) not null,
+    email varchar(200) not null,
     user_name varchar(100) not null,
-    user_birthday varchar(8) not null,
+    user_birthday date not null,
 	user_permission enum('office', 'customer', 'employeer') not null
 );
 
 create table employeer(
 	id_employeer int not null auto_increment,
     
-	primary key (id_employeer),
-    constraint id_employeer foreign key(id_employeer) references usuario(id_user)
+	primary key (id_employeer)
+    # constraint id_employeer foreign key(id_employeer) references user(id_user)
 );
 
 create table customer(
 	id_customer int not null auto_increment,
     client_name char(255) not null,
     
-    primary key (id_customer),
-    constraint id_customer foreign key(id_customer) references usuario(id_user)
+    primary key (id_customer)
+    # constraint id_customer foreign key(id_customer) references user(id_user)
 );
 
 create table employeer_gets_calls(
@@ -28,16 +32,12 @@ create table employeer_gets_calls(
 	fk_id_employeer int not null,
     fk_id_customer int not null,
 	description varchar(1000) not null,
-    stage enum('closed', 'open') not null,
+    status enum('closed', 'open') not null
     
-    constraint fk_id_employeer foreign key(fk_id_employeer) references employeer(id_employeer),
-    constraint fk_id_customer foreign key(fk_id_customer) references customer(id_customer)
+   # constraint fk_id_employeer foreign key(fk_id_employeer) references employeer(id_employeer),
+   # constraint fk_id_customer foreign key(fk_id_customer) references customer(id_customer)
 );
 
-#drop database RH;
-
-insert into usuario(user_name, user_birthday, user_permission) values("Osmar", "20040813", "employeer");
-insert into employeer(user_name, user_birthday, user_permission) values("Osmar", "20040813", "employeer");
+insert into user(user_name, user_birthday, user_permission, password, email) values("Osmar", "20040813", "employeer", "123", "email@teste");
  
- select * from usuario;
- select * from employeer
+ select * from user;
