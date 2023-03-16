99030792d9b5ad19af58d7384922a27c78aaa5db
Alunos <aluno.etec@etecEmbu.com.br>
2023-03-15 21:18:46 -0300

    Changing dataBase

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,16 +3,9 @@ import mysql2  from 'mysql2/promise';
 const connection = async () => 
 {
   try {
-  const connection = await mysql2.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.DB_PASSWORD,
-    database: process.DB_NAME,
-    uri: process.env.DB_URI
-  });
+    const connection = await mysql2.createConnection(process.env.DATABASE_URL);
 
-  return connection;
+    return connection;
   } 
   catch (error) {
     console.log(error);
